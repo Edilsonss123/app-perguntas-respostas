945844214924be1a0447732c61affc41b157ed25
Edilsonss123 <edilson016@outook.com>
2020-03-28 08:56:34 -0300

    iniciando com ejs

--- src/serve.ts
+++ src/serve.ts
@@ -1,1 +1,18 @@
-console.log('Primeiro passo com node');
+import express from 'express';
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/:nome/:linguagem', (req, res) => {
+    const objeto:Object = {
+        nome:req.params.nome??'Não informado', 
+        linguagem: req.params.linguagem??'Não informado', 
+        empresa: 'Infornet', 
+        inscritos: 1900
+    };
+    return res.render('index', objeto);
+});
+
+app.listen(1818, _=> {
+    console.log('App rodando');
+});
